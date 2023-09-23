e32ddf6e4a95ede2fa6ea15b4d863a41ed739c4c
Nikhil Narvekar <nikhil.narvekar.712@gmail.com>
2023-09-23 18:22:01 -0500

    getting all event, news, weather, disaster data from db/api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,76 @@ import FeedCard from './FeedCards';
 import InfiniteScroll from "react-infinite-scroll-component";
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { ref, set, push, update, child, onValue } from "firebase/database";
+import { db } from "./firebase";
+
+import queryString from 'query-string';
+import moment from 'moment';
 
 function App() {
 
   const [allCards, setAllCards] = useState([1, 2, 3])
-  const [temp, setTemp] = useState([])
+  const [allData, setAllData] = useState([])
   const [isLocalToggle, setIsLocalToggle] = useState(true)
 
   // get disaster, news, and event data from firebase
   useEffect(() => {
+    
+    // do for first user in list
+    const vref = ref(db, 'users');
+    let user = [];
+    let loc = [];
+    onValue(vref, (snapshot) => {
+      const data = Object.values(snapshot.val());
+      user = data[0];
+      loc[0] = user['location']['lat']
+      loc[1] = user['location']['long']
+    });
 
+    // get data from tomorrow
+    const getTimelineURL = "https://api.tomorrow.io/v4/timelines";
+    const apikey = ['uUCcNwTXpst3ab0MUXl8G83bgRN9uOLU'];
+    let location = loc.join(",");
+    const fields = ["precipitationIntensity", "windSpeed", "temperature"];
+    const units = "imperial";
+    const timesteps = ["current"];
+    const now = moment.utc();
+    const startTime = moment.utc(now).add(0, "minutes").toISOString();
+    const timezone = "America/Chicago";
+    const getTimelineParameters = queryString.stringify(
+    {
+      apikey,
+      location,
+      fields,
+      units,
+      timesteps,
+      startTime,
+      timezone,
+    });
 
+    let tomorrowdata = []
+    axios.get(getTimelineURL + '?' + getTimelineParameters).then(res => {
+      tomorrowdata = res.data;
+    }).catch((e) => console.log(e))
 
+    // get mock feed data
+    const bref = ref(db, 'feed');
+    let mockdata = [];
+    onValue(bref, (snapshot) => {
+      const data = Object.values(snapshot.val());
+      mockdata = data;
+    });
+
+    // get flood data
+    const cref = ref(db, 'dashboard_floods');
+    let flooddata = [];
+    onValue(cref, (snapshot) => {
+      const data = Object.values(snapshot.val());
+      flooddata = data;
+    });
+
+    console.log(mockdata)
+    console.log(flooddata)
   }, [])
 
   // update the feed whenever toggle is pressed
